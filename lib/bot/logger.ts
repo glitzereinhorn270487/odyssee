de095209be17b6056f9f17319858961bbd6e15cd
glitzereinhorn270487 <glitzereinhorn270487@gmail.com>
2025-08-14 23:01:48 +0200

    Update logger.ts

--- lib/bot/logger.ts
+++ lib/bot/logger.ts
@@ -2,7 +2,7 @@ export function log(tag: string, payload: any) {
   try {
     console.log(`[${tag}]`, JSON.stringify(payload));
   } catch {
-    console.log(`[${tag}]`, payloadi);
+    console.log(`[${tag}]`, payload);
   }
 }
 
